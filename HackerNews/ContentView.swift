c6010600447f4686d741eca63aff618e6fc2c0e1
Sean O'Connor <so@seano.dev>
2023-07-02 14:21:26 -0600

    Create FeedView

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             TabView {
-                Text("Feed")
+                FeedView()
                     .tabItem {
                         Image(systemName: "newspaper")
                             .font(.system(size: 24))
---
+++ HackerNews/Views/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  HackerNews
+//
+//  Created by Sean O'Connor on 7/2/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("Feed")
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
