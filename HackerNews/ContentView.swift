6fbb994b472ae88bc7d5c401544bbf09f5eba828
Sean O'Connor <so@seano.dev>
2023-07-02 13:51:17 -0600

    Initial commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Sean O'Connor on 7/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                Text("Feed")
+                    .tabItem {
+                        Image(systemName: "newspaper")
+                            .font(.system(size: 24))
+                    }
+
+                Text("Search")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                            .font(.system(size: 24))
+                    }
+
+                Text("Account")
+                    .tabItem {
+                        Image(systemName: "person")
+                            .font(.system(size: 24))
+                    }
+
+                Text("Settings")
+                    .tabItem {
+                        Image(systemName: "gear")
+                            .font(.system(size: 24))
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Sean O'Connor on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
