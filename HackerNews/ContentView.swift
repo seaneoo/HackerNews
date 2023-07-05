f8f68f3e1eb1d8530d0dd72ae098623e19965cb2
Sean O'Connor <so@seano.dev>
2023-07-05 17:27:56 -0600

    Detect tabs being changed

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -8,31 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 1
+
+    var handler: Binding<Int> { Binding(
+        get: { self.selectedTab },
+        set: {
+            self.selectedTab = $0
+        }
+    ) }
+
     var body: some View {
-        TabView {
+        TabView(selection: handler) {
             FeedView()
                 .tabItem {
                     Image(systemName: "newspaper")
                         .font(.system(size: 24))
-                }
+                }.tag(1)
 
             SearchView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                         .font(.system(size: 24))
-                }
+                }.tag(2)
 
             AccountView()
                 .tabItem {
                     Image(systemName: "person")
                         .font(.system(size: 24))
-                }
+                }.tag(3)
 
             SettingsView()
                 .tabItem {
                     Image(systemName: "gear")
                         .font(.system(size: 24))
-                }
+                }.tag(4)
+        }.onChange(of: selectedTab) { _ in
+            AppConstants.feedbackGenerator.impactOccurred()
         }
     }
 }
