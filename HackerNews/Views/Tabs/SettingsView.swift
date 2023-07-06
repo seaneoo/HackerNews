97863c73a5c1c75faca0d023e360fe067b0e7729
Sean O'Connor <so@seano.dev>
2023-07-05 18:16:20 -0600

    Update Settings view

--- HackerNews/Views/Tabs/SettingsView.swift
+++ HackerNews/Views/Tabs/SettingsView.swift
@@ -11,8 +11,36 @@ struct SettingsView: View {
     var body: some View {
         NavigationStack {
             Form {
-                Section(header: Text("About")) {
-                    Text("Version \(UIApplication.appVersion ?? "0").\(UIApplication.buildVersion ?? "0")")
+                Section {
+                    NavigationLink {
+                        Text("General")
+                    } label: {
+                        Label("General", systemImage: "gear")
+                    }
+
+                    NavigationLink {
+                        Text("Appearance")
+                    } label: {
+                        Label("Appearance", systemImage: "theatermasks")
+                    }
+                } header: {
+                    Text("App Settings")
+                }
+
+                Section {
+                    Link(destination: URL(string: "https://github.com/seaneoo/HackerNews/")!) {
+                        Label("Source Code", systemImage: "curlybraces")
+                    }
+
+                    Link(destination: URL(string: "https://github.com/seaneoo/HackerNews/issues")!) {
+                        Label("Issues", systemImage: "exclamationmark.circle")
+                    }
+                } header: {
+                    Text("About")
+                } footer: {
+                    if let version = UIApplication.appVersion, let build = UIApplication.buildVersion {
+                        Text("Version \(version) (Build \(build))")
+                    }
                 }
             }
             .navigationTitle("Settings")
