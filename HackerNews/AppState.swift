615cad04208034e8294d7498affdaa20b3ad6f2c
Sean O'Connor <so@seano.dev>
2023-07-07 16:56:10 -0600

    Update comments and code style

--- HackerNews/AppState.swift
+++ HackerNews/AppState.swift
@@ -8,12 +8,18 @@
 import Foundation
 
 class AppState: ObservableObject {
+    // MARK: Alert message box
+
     @Published var isShowingAlert: Bool = false
     @Published var alertTitle: String = ""
     @Published var alertMessage: String = ""
 
+    // MARK: Informational toast
+
     @Published var isShowingToast: Bool = false
     @Published var toastMessage: String = ""
+
+    // MARK: Error toast
 
     @Published var isShowingError: Bool = false
     @Published var errorMessage: String = ""
--- HackerNews/HackerNewsApp.swift
+++ HackerNews/HackerNewsApp.swift
@@ -10,12 +10,14 @@ import SwiftUI
 
 @main
 struct HackerNewsApp: App {
+    // Global app state, controls the alert box(es) and toast(s)
     @StateObject var appState = AppState()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(appState)
+                // Alert message box
                 .alert(appState.alertTitle, isPresented: $appState.isShowingAlert) {
                     Button(role: .cancel) {
                         appState.isShowingAlert.toggle()
@@ -25,9 +27,11 @@ struct HackerNewsApp: App {
                 } message: {
                     Text(appState.alertMessage)
                 }
+                // Informational toast
                 .toast(isPresenting: $appState.isShowingToast) {
                     AlertToast(displayMode: .hud, type: .regular, title: appState.toastMessage)
                 }
+                // Error toast
                 .toast(isPresenting: $appState.isShowingError) {
                     AlertToast(displayMode: .alert, type: .error(.red), title: appState.errorMessage)
                 }
